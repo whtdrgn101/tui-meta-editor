2e27faee80f8fc6f10dfb6b73c90aebd2ab87c26
Timothy DeWees <timothy.dewees@gmail.com>
2025-03-13 14:12:49 -0400

    Switched to OS agnostic path building

--- media/renamer.py
+++ media/renamer.py
@@ -6,13 +6,12 @@ class MediaRenamer:
     """Class for renaming media files based on patterns."""
     title = ""
     
-
     def __init__(self, title):
        self.title = title
     
     def rename_file(self, file_path: str, season: int, episode: int) -> bool:
         path, new_filename = self.generate_new_name(file_path, season, episode)
-        os.rename(file_path, f"{path}/{new_filename}")
+        os.rename(file_path, os.path.join(path, new_filename))
         return True
     
     def generate_new_name(self, file_path: str, season: int, episode: int) -> str:
